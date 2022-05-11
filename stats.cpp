f5b4b501b534f386edc4b225a6b4c3aa4fa1572e
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-05-11 15:52:20 +0530

    updated changes for ComputeStatistics function

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,22 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>vec& ) {
+    Stats Stats1;
+    float sum = 0.0;
+    float min = vec[0];
+    float max = vec[0];
+    
+    for(int i =0; i<vec.size(); i++) {
+        if(min>vec[i]) {
+            min = vec[i];
+        } else if(max<vec[i]) {
+            max = vec[i];
+        }
+        
+        sum = sum + vec[i] ;
+        Stats1.average = (sum)/vec.size();
+        Stats1.min = min;
+        Stats1.max = max;
+        
+     }
 }
