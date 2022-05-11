dba9144acb802968d730bd839d70f91b69fa3eb7
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-05-11 22:25:38 +0530

    update stats.h file

--- stats.h
+++ stats.h
@@ -5,6 +5,24 @@ struct Stats {
     float min;
     float max;
 };
+
+struct EmailAlert {
+    bool emailSent;
+ 
+};
+
+struct LEDAlert {
+    bool ledGlows;
+};
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>&vec);
-}
+};
+class StatsAlerter{
+    public:
+        float m_maxthreshold;
+        struct EmailAlert emailAlert;
+        struct LEDAlert ledAlert;
+        StatsAlerter(float maxthreshold, struct EmailAlert emailAlert,
+                 struct LEDAlert ledAlert);
+        void checkAndAlert(const vector<float>&vec) ;
+};
